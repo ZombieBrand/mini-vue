0ea69929827d59f5a838e3b3efcb8fe4142649e4
ZombieBrand <zombiebrandg@gmail.com>
2022-12-06 17:43:23 +0800

    feat: 增加rollup打包mini-vue
    Changes to be committed:
    	modified:   example/helloworld/app.js
    	modified:   example/helloworld/main.js
    	new file:   lib/guide-mini-vue.cjs.js
    	new file:   lib/guide-mini-vue.esm.js
    	modified:   package.json
    	modified:   pnpm-lock.yaml
    	new file:   rollup.config.js
    	new file:   src/index.ts
    	modified:   src/runtime-core/index.ts
    	modified:   tsconfig.json

--- example/helloworld/app.js
+++ example/helloworld/app.js
@@ -1,19 +1,20 @@
+import { h } from '../../lib/guide-mini-vue.esm.js'
+
 // 这里就是template经过编译后，得到的根组件组合对象（如果用户使用optionsAPI，测绘得到根组件选项对象），里面会包含一个render()函数
 export default {
-    render() {
-      return h('div', {
-        id: 'root',
-        class: ['flex', 'container-r']
-      }, [
-        h('p', {class: 'red'}, 'red'),
-        h('p', {class: 'blue'}, 'blue')
-      ])
-    },
-    setup() {
-      // 返回对象或者h()渲染函数
-      return {
-        name: 'hi my app'
-      }
+  render() {
+    return h('div', {
+      id: 'root',
+      class: ['flex', 'container-r']
+    }, [
+      h('p', { class: 'red' }, 'red'),
+      h('p', { class: 'blue' }, 'blue')
+    ])
+  },
+  setup() {
+    // 返回对象或者h()渲染函数
+    return {
+      name: 'hi my app'
     }
   }
-  
+}
