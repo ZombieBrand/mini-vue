1b951a9706987c60c7e62a54217a919fb62d8135
ZombieBrand <zombiebrandg@gmail.com>
2022-12-20 17:34:19 +0800

    fix: 删除log代码 Changes to be committed: 	modified:   src/runtime-core/componentEmit.ts

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -5,8 +5,8 @@ export function emit(instance, event, ...args) {
     const { props } = instance
     // props中触发事件
     const handlerName = toHandlerKey(camelize(event))
-    console.log(camelize(event),handlerName,1111)
+
     const handler = props[handlerName]
-    console.log({ handler })
+
     handler && handler(...args)
 }
